a8599f24f331fc639a81f7ffa9fde1b03875cc68
ronaksharma-simform <ronak.sharma@simformsolutions.com>
2026-03-19 16:56:54 +0530

    feat: Add postfix Evaluation class

---
+++ src/utils/postfixEvaluation.ts
@@ -0,0 +1,45 @@
+import {Stack} from "./stack.js";
+import { operators, functions } from "./operations.js";
+import * as Type from "@/types";
+export default class PostfixEvaluator implements Type.IPostfixEvaluation{
+    operators: Type.TOperations;
+    functions: Type.TFunctions;
+	constructor() {
+		this.operators = operators;
+		this.functions = functions;
+	}
+	evaluate(tokens:string[]) {
+		if (!Array.isArray(tokens) || tokens.length === 0) {
+			throw new Error("Invalid expression");
+		}
+		const st = new Stack();
+		for (let token of tokens) {
+			if (this.operators.has(token) || this.functions.has(token)) {
+				const currentOperands:number[] = [];
+				// get current operator or function details
+				let currentOperator = this.operators.has(token)
+					? this.operators.get(token)!
+					: this.functions.get(token)! ;
+
+				if (st.size() < currentOperator.arity) {
+					throw new Error("Malformed Expression");
+				}
+
+				for (let i = 0; i < currentOperator.arity; i++) {
+					currentOperands.unshift(parseFloat(st.pop()));
+				}
+				// executing operator on operands
+				let result = currentOperator.execute(...currentOperands);
+
+				st.push(String(result));
+			} else {
+				st.push(token);
+			}
+		}
+		console.log(st.peek());
+		if (st.size() !== 1) {
+			throw new Error("Malformed Expression");
+		}
+		return st.peek();
+	}
+}
